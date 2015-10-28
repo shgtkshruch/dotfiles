2ec16b48109b02a50f394b7a2bdd38f5c2e619f2
shgtkshruch <shgtk.shruch@gmail.com>
2015-10-29 00:18:37 +0900

    tweaks

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-test -z $(which brew) && ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" && brew instll ansible
+test -z $(which brew) && ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" && brew install ansible
 ansible-playbook playbook.yml -i host
